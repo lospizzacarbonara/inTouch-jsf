405ce643ca022171462702616327e779098b2c33
jfaldanam <jfaldanam@gmail.com>
2019-05-20 09:06:36 +0200

    Remove description size limit on social group

--- inTouch-jsf-ejb/src/java/inTouch/entity/SocialGroup.java
+++ inTouch-jsf-ejb/src/java/inTouch/entity/SocialGroup.java
@@ -59,7 +59,7 @@ public class SocialGroup implements Serializable {
     @Column(name = "creationDate")
     @Temporal(TemporalType.DATE)
     private Date creationDate;
-    @Size(max = 45)
+    @Size(max = 500)
     @Column(name = "description")
     private String description;
     @Column(name = "type")
